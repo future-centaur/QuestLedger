ae846fa4f2150bea07a988d5bde8ffd3e65ac513
future-centaur <munkev001@gmail.com>
2026-09-04 14:53:03 +0300

    chore: include AppDeploy realtime adapter

---
+++ backend/realtime.ts
@@ -0,0 +1,2 @@
+import { removeSubscriptionsByConnection } from './realtime-subscribers';
+export const realtime = async (event:any) => { let msg:any={}; try { msg=JSON.parse(event.body||'{}'); } catch {} if(msg.type==='system.connected') return {statusCode:200}; if(msg.type==='system.disconnected'){const id=msg.payload?.connection_id;if(id) await removeSubscriptionsByConnection(id);return {statusCode:200}} return {statusCode:200}; };
